fff52f4fcc6cbbeeb36a44496635f51e81f28b53
Niko Kivelä <niko@tovrleaf.com>
2019-01-07 21:05:56 +0200

    Add flake8 as lint and run it

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-import requests
 import conf.api_config as config
 from apiclient.coc_api import CocApi
 
-bearer_token=config.bearer
-api_endpoint=config.endpoint
+
+bearer_token = config.bearer
+api_endpoint = config.endpoint
 
 api = CocApi(bearer_token, api_endpoint, 'v1')
 print api.get_clan('LCC8CL')
--- apiclient/coc_api.py
+++ apiclient/coc_api.py
@@ -1,26 +1,28 @@
 import requests
 
+
 class CocApi:
 
-  def __init__(self, bearer, endpoint, endpoint_version):
-    self.bearer = bearer
-    self.endpoint = endpoint
-    self.endpoint_version = endpoint_version
+    def __init__(self, bearer, endpoint, endpoint_version):
+        self.bearer = bearer
+        self.endpoint = endpoint
+        self.endpoint_version = endpoint_version
 
-  def get_clan(self ,tag):
-    r = requests.get(
-      self.__build_uri('clans') + '/%23{}'.format(tag),
-      headers=self.__build_headers())
-    return r.content
+    def get_clan(self, tag):
+        r = requests.get(
+            self.__build_uri('clans') + '/%23{}'.format(tag),
+            headers=self.__build_headers())
+        return r.content
 
-  def get_player(self, tag):
-    r = requests.get(
-      self.__build_uri('players') + '/%23{}'.format(tag),
-      headers=self.__build_headers())
-    return r.content
+    def get_player(self, tag):
+        r = requests.get(
+            self.__build_uri('players') + '/%23{}'.format(tag),
+            headers=self.__build_headers())
+        return r.content
 
-  def __build_uri(self, section):
-    return self.endpoint + '/' + section
+    def __build_uri(self, section):
+        return self.endpoint + '/' + section
 
-  def __build_headers(self):
-    return {"Accept": "application/json", "authorization": "Bearer {}".format(self.bearer)}
+    def __build_headers(self):
+        return {"Accept": "application/json",
+                "authorization": "Bearer {}".format(self.bearer)}
