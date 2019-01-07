ca27b5b31bd47dff451b910b9f24f93a5cd5b5d3
Niko Kivelä <niko@tovrleaf.com>
2019-01-07 18:38:58 +0200

    Move API communication to its own class

--- api.py
+++ api.py
@@ -1,26 +1,9 @@
 import requests
 import conf.api_config as config
+from apiclient.coc_api import CocApi
 
 bearer_token=config.bearer
 api_endpoint=config.endpoint
 
-def build_headers():
-  return {"Accept": "application/json", "authorization": "Bearer {}".format(bearer_token)}
-
-def build_uri(section):
-  return api_endpoint + '/' + section
-
-
-def get_clan(tag):
-  r = requests.get(
-    build_uri('clans') + '/%23{}'.format(tag),
-    headers=build_headers())
-  print r.content
-
-def get_player(tag):
-  r = requests.get(
-    build_uri('players') + '/%23{}'.format(tag),
-    headers=build_headers())
-  print r.content
-
-get_clan('LCC8CL')
+api = CocApi(bearer_token, api_endpoint, 'v1')
+print api.get_clan('LCC8CL')
