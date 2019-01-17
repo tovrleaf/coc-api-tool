db0431aecb9f1db091b2debeda753f95da1a6221
Niko Kivelä <niko@tovrleaf.com>
2019-01-17 22:44:45 +0200

    Refactory making request

--- apiclient/coc_api.py
+++ apiclient/coc_api.py
@@ -9,16 +9,17 @@ class CocApi(object):
         self.endpoint_version = endpoint_version
 
     def get_clan(self, tag):
-        r = requests.get(
-            self.__build_uri('clans') + '/%23{}'.format(tag),
-            headers=self.__build_headers())
+        r = self.__make_request('clans', tag)
         return r.content
 
     def get_player(self, tag):
-        r = requests.get(
-            self.__build_uri('players') + '/%23{}'.format(tag),
-            headers=self.__build_headers())
+        r = self.__make_request('players', tag)
         return r.content
+
+    def __make_request(self, section, tag):
+        return requests.get(
+            self.__build_uri(section) + '/%23{}'.format(tag),
+            headers=self.__build_headers())
 
     def __build_uri(self, section):
         return self.endpoint + '/' + self.endpoint_version + '/' + section
